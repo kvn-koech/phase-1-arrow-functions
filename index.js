a3536f2dc2a11e8cc1fe94425e180a0e8c59aabb
Kevin Koech <kevinkoech007@gmail.com>
2025-06-12 18:26:24 +0300

    complete test

---
+++ index.js
@@ -0,0 +1,17 @@
+//    1) has a function expression called divide
+//    2) divide divides 2000 by 100
+
+const divide = function() {
+  return 2000 / 100;
+};
+
+//     3) has an arrow function called square
+//     4) square arrow function takes one parameter and multiplies it times itself
+
+const square = (x) => x * x;
+
+
+//     5) has an arrow function called add
+//     6) add arrow function takes two parameters and adds them together
+
+const add = (a, b) => a + b;
